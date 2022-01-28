2e2831d7566562ab72ffca3f4e91099f65131fc6
Caio Bukvar <bukvar2009@gmail.com>
2022-01-28 15:38:04 -0300

    minor fix name database

--- databases.sql
+++ databases.sql
@@ -1,4 +1,4 @@
-create table usuarios (
+create table users (
   id serial primary key,
   name text,
   username text not null unique,
--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const login = require('./controllers/login');
 const verifyToken = require('./helpers/verifyToken');
 
-
 const rotas = express();
 
 //cadastro usuário
@@ -10,16 +9,5 @@ rotas.post('/cadastro', usuarios.cadastrarUsuario);
 
 //Login
 rotas.post('/login', login.login);
-
-// filtro de verificação (middleware) - usuário está logado?
 rotas.use(verifyToken);
 
-// obter e atualizar perfil do usuário logado
-rotas.get('/perfil', usuarios.obterPerfil);
-rotas.put('/perfil', usuarios.atualizarPerfil);
-
-// Postagens
-rotas.post('/postagens', postagens.novaPostagem);
-rotas.get('/postagens', postagens.feed);
-rotas.post('/postagens/:postagemId/curtir', postagens.curtir);
-rotas.post('/postagens/:postagemId/comentar', postagens.comentar);
