e0af9b6c485b23e604c04df6ed30e22d6448cc8d
Caio Bukvar <bukvar2009@gmail.com>
2022-01-28 15:08:48 -0300

    creation sql

---
+++ databases.sql
@@ -0,0 +1,22 @@
+create table usuarios (
+  id serial primary key,
+  name text,
+  username text not null unique,
+  cpf text unique,
+  email text unique,
+  phone text,
+  genero text,
+  password text not null,
+  confirmPassword text not null,
+  adress text,
+  );
+
+create table products (
+  id serial primary key,
+  name text not null,
+  price text not null,
+  stock int not null,
+  category text not null,
+  );
+
+
